ce2dcb19e2d07752878f7aa12653c517f3b46cb9
SaharaSurfer <st119029@student.spbu.ru>
2024-03-18 20:57:11 +0300

    Implement operator+ and overflow handling in FixedPoint class
    Add overflow handling to the operator+ overload in the FixedPoint class.
    Before performing addition, check for potential overflow scenarios to prevent
    undefined behavior. If the result exceeds the maximum value representable by
    int32_t, throw an std::overflow_error exception to indicate that the maximum
    value has been exceeded.
    
    Signed-off-by: SaharaSurfer <st119029@student.spbu.ru>

--- src/fixed_point.cc
+++ src/fixed_point.cc
@@ -1,9 +1,27 @@
 #include "header/fixed_point.h"
 
+#include <cstdint>
+#include <stdexcept>
+#include <limits>
+
 FixedPoint::FixedPoint(float value) {
   value_ = static_cast<int32_t>(value * (1 << fraction_bits_));
 }
 
+FixedPoint::FixedPoint(int32_t value) {
+  value_ = value;
+}
+
 float FixedPoint::GetFloat() {
   return static_cast<float>(value_) / (1 << fraction_bits_);
+}
+
+FixedPoint FixedPoint::operator+(FixedPoint other) {
+  int64_t result = value_ + other.value_;
+
+  if (result > std::numeric_limits<int32_t>::max()) {
+    throw std::overflow_error("The maximum value in a fixed-point number exceeded!");
+  }
+
+  return FixedPoint(static_cast<int32_t>(result));
 }
--- src/header/fixed_point.h
+++ src/header/fixed_point.h
@@ -6,11 +6,14 @@
 class FixedPoint {
  public:
   FixedPoint(float value);
+  FixedPoint(int32_t value);
 
   float GetFloat();
 
+  FixedPoint operator+(FixedPoint other);
+
  private:
-  static const uint8_t fraction_bits_ = 15;
+  static const uint8_t fraction_bits_ = 16;
 
   int32_t value_;
 };
