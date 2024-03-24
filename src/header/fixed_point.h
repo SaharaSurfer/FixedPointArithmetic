4f91666a6b2af19c190de9ac291988695b8a5231
SaharaSurfer <st119029@student.spbu.ru>
2024-03-24 12:16:59 +0300

    Make the FixedPoint class a template class
    The FixedPoint class is made templated so that it is possible to change
    the accuracy of the calculation. Added functionality for correct addition,
    subtraction and so on between two FixedPoint objects of different accuracy.
    
    Signed-off-by: SaharaSurfer <st119029@student.spbu.ru>

--- src/header/fixed_point.h
+++ src/header/fixed_point.h
@@ -2,28 +2,133 @@
 #define FIXEDPOINTARITHMETIC_HEADER_FIXED_POINT_H_
 
 #include <cstdint>
+#include <cmath>
+#include <stdexcept>
 
+template<uint8_t fraction_bits=16>
 class FixedPoint {
  public:
-  FixedPoint(float value);
+  FixedPoint(float value) {
+    int64_t result = static_cast<int64_t>(
+        std::round(static_cast<double>(value) * (1 << fraction_bits)));
 
-  float GetFloat();
+    CheckOverUnderFlow(result);
 
-  FixedPoint& operator+=(const FixedPoint& other);
-  FixedPoint& operator-=(const FixedPoint& other);
-  FixedPoint& operator*=(const FixedPoint& other);
-  FixedPoint& operator/=(const FixedPoint& other);
+    value_ = static_cast<int32_t>(result);
+  }
 
-  FixedPoint operator+(const FixedPoint& other) const;
-  FixedPoint operator-() const;
-  FixedPoint operator-(const FixedPoint& other) const;
-  FixedPoint operator*(const FixedPoint& other) const;
-  FixedPoint operator/(const FixedPoint& other) const;
+  float GetFloat() const {
+    return static_cast<float>(value_) / (1 << fraction_bits);
+  }
+
+  int32_t GetUnderlyingInt() const {
+    return value_;
+  }
+
+  template<uint8_t other_fb>
+  FixedPoint& operator+=(const FixedPoint<other_fb>& other) {
+    int64_t result = static_cast<int64_t>(value_) + GetScaledValue(other);
+
+    CheckOverUnderFlow(result);
+
+    value_ = static_cast<int32_t>(result);
+    return *this;
+  }
+
+  template<uint8_t other_fb>
+  FixedPoint& operator-=(const FixedPoint<other_fb>& other) {
+    int64_t result = static_cast<int64_t>(value_) - GetScaledValue(other);
+
+    CheckOverUnderFlow(result);
+
+    value_ = static_cast<int32_t>(result);
+    return *this;
+  }
+
+  template<uint8_t other_fb>
+  FixedPoint& operator*=(const FixedPoint<other_fb>& other) {
+    int64_t result = (static_cast<int64_t>(value_) *
+        other.GetUnderlyingInt()) >> other_fb;
+
+    CheckOverUnderFlow(result);
+
+    value_ = static_cast<int32_t>(result);
+    return *this;
+  }
+
+  template<uint8_t other_fb>
+  FixedPoint& operator/=(const FixedPoint<other_fb>& other) {
+    int64_t result = (static_cast<int64_t>(value_) + (other.GetUnderlyingInt() >> 1)) /
+        other.GetUnderlyingInt() << other_fb;
+
+    CheckOverUnderFlow(result);
+
+    value_ = static_cast<int32_t>(result);
+    return *this;
+  }
+
+  template<uint8_t other_fb>
+  FixedPoint operator+(const FixedPoint<other_fb>& other) const {
+    FixedPoint<fraction_bits> result = *this;
+    result += other;
+
+    return result;
+  }
+
+  FixedPoint operator-() const {
+    FixedPoint<fraction_bits> result = *this;
+    result.value_ = -result.value_;
+    
+    return result;
+  }
+
+  template<uint8_t other_fb>
+  FixedPoint operator-(const FixedPoint<other_fb>& other) const {
+    FixedPoint<fraction_bits> result = *this;
+    result -= other;
+
+    return result;
+  }
+
+  template<uint8_t other_fb>
+  FixedPoint operator*(const FixedPoint<other_fb>& other) const {
+    FixedPoint<fraction_bits> result = *this;
+    result *= other;
+
+    return result;
+  }
+
+  template<uint8_t other_fb>
+  FixedPoint operator/(const FixedPoint<other_fb>& other) const {
+    FixedPoint<fraction_bits> result = *this;
+    result /= other;
+
+    return result;
+  }
 
  private:
-  static const uint8_t kFractionBits = 16;
+  template<uint8_t other_fb>
+  constexpr int32_t GetScaledValue(const FixedPoint<other_fb>& other) const {
+    int64_t result = other.GetUnderlyingInt();
 
-  void CheckOverUnderFlow(int64_t value) const;
+    if constexpr (fraction_bits > other_fb) {
+      result <<= (fraction_bits - other_fb);
+    } else if constexpr (fraction_bits < other_fb) {
+      result >>= abs(fraction_bits - other_fb);
+    }
+
+    return result;
+  }
+
+  constexpr void CheckOverUnderFlow(int64_t value) const {
+    if (value > std::numeric_limits<int32_t>::max()) {
+      throw std::overflow_error("Overflow in FixedPoint arithmetic.");
+    }
+
+    if (value < std::numeric_limits<int32_t>::min()) {
+      throw std::underflow_error("Underflow in FixedPoint arithmetic.");
+    }
+  }
 
   int32_t value_;
 };
--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,20 @@
+#include <cmath>
 #include <iostream>
 
 #include "header/fixed_point.h"
 
 int main() {
-  FixedPoint a(0.125f);
-  FixedPoint b(0.375f);
+  FixedPoint<24> a(1.23f);
+  FixedPoint<8> b(6.25f);
 
-  auto c = a / b;
-  std::cout << c.GetFloat();
+  auto c = a * b;
+  std::cout << c.GetFloat() << std::endl;
+
+  FixedPoint<24> d(std::sqrt(2));
+  std::cout << d.GetFloat() << std::endl;
+
+  FixedPoint<8> e(12345.6789f);
+  std::cout << e.GetFloat() << std::endl;
 
   return 0;
 }
